4c6e97ff3c1edabce49980db373b15ef0995d908
Amine Ben Asker <ben.asker.amine@gmail.com>
2019-02-20 04:26:53 +0100

    Update task state and persistence classes

--- atr/persistences/task.py
+++ atr/persistences/task.py
@@ -5,6 +5,7 @@ class Task(Document):
     module = StringField(required=True)
     _id = StringField(required=True, primary_key=True)
     status = StringField(required=True)
+    args = DictField(required=False)
     time_start = DateTimeField(required=True)
     time_finish = DateTimeField(required=False)
     time = FloatField(required=False)
--- atr/utils/state.py
+++ atr/utils/state.py
@@ -1,45 +1,11 @@
-class State:
-    def __init__(self):
-        self.state = Pending
-
-    def switch(self, new_state):
-        if not issubclass(new_state, BaseState):
-            raise TypeError("New State '{}' is not a subclass of '{}'".format(new_state, BaseState))
-        if new_state == self.state:
-            pass
-        elif new_state in self.state.allowed:
-            self.state = new_state
-        else:
-            raise TypeError(
-                "Current state could not be switched from {} to  {}".format(self.state.__name__, new_state.__name__))
-
-    def success(self):
-        self.switch(Success)
-
-    def failed(self):
-        self.switch(Failed)
-
-    def error(self):
-        self.switch(Error)
-
-    def running(self):
-        self.switch(Running)
-
-    def pending(self):
-        self.switch(Pending)
-
-    def initializing(self):
-        self.switch(Initializing)
+class BaseState:
+    allowed = []
 
     def __str__(self):
-        return self.state.__name__
+        return self.__name__
 
     def __dict__(self):
         return self.__str__()
-
-
-class BaseState:
-    allowed = []
 
 
 class Success(BaseState):
@@ -64,3 +30,75 @@ class Initializing(BaseState):
 
 class Pending(BaseState):
     allowed = [Initializing, Error]
+
+
+class State:
+    Pending = Pending
+    Initializing = Initializing
+    Running = Running
+    Error = Error
+    Failed = Failed
+    Success = Success
+
+    def __init__(self, state):
+        self.state = None
+        if not state:
+            self.state = Pending
+        elif isinstance(state, str):
+            self.from_string(state)
+        elif issubclass(state, BaseState):
+            self.state = state
+        else:
+            raise TypeError("New State '{}' is not a subclass of '{}'".format(state, BaseState))
+
+    def from_string(self, status):
+        state_cls = getattr(self, status, None)
+        if state_cls:
+            if self.state:
+                self.switch(state_cls)
+            else:
+                self.state = state_cls
+        else:
+            raise TypeError("State '{}' is not defined".format(status))
+
+    def is_final(self):
+        return len(self.state.allowed) == 0
+
+    def is_equal(self, state):
+        return self.state ==  state
+
+    def switch(self, new_state):
+        if not issubclass(new_state, BaseState):
+            raise TypeError("New State '{}' is not a subclass of '{}'".format(new_state, BaseState))
+        if new_state == self.state:
+            pass
+        elif new_state in self.state.allowed:
+            self.state = new_state
+        else:
+            raise TypeError(
+                "Current state could not be switched from {} to  {}"
+                "".format(self.state.__name__, new_state.__name__))
+
+    def success(self):
+        self.switch(Success)
+
+    def failed(self):
+        self.switch(Failed)
+
+    def error(self):
+        self.switch(Error)
+
+    def running(self):
+        self.switch(Running)
+
+    def pending(self):
+        self.switch(Pending)
+
+    def initializing(self):
+        self.switch(Initializing)
+
+    def __str__(self):
+        return self.state.__name__
+
+    def __dict__(self):
+        return self.__str__()
